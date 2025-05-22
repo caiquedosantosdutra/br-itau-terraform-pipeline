38361d91a11e5856ee5a9932b78b3e6513ce2f1b
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 12:51:35 -0300

    fixing main

--- main.tf
+++ main.tf
@@ -1,26 +1,41 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "caique-s3" {
   bucket = "pipeline-tf-githubactions"
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "pipeline-tf-githubactions-logs"
+  
+  acl = "log-delivery-write"
+
+  # Optional: to block public access on log bucket as well
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "caique-ownership-controls" {
   bucket = aws_s3_bucket.example.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "caique-acl" {
   depends_on = [aws_s3_bucket_ownership_controls.example]
 
   bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_public_access_block" "caique-public-access-block" {
   bucket = aws_s3_bucket.example.id
 
   block_public_acls       = true
@@ -42,7 +57,7 @@ resource "aws_kms_key" "mykey" {
   deletion_window_in_days = 7
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "caique-encryption" {
   bucket = aws_s3_bucket.example.id
 
   rule {
@@ -66,6 +81,17 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "example" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+
+  root_block_device {
+    encrypted   = true
+    volume_size = 8      # Optional: adjust as needed
+    volume_type = "gp3"  # Optional: adjust as needed
+  }
+
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
 
   tags = {
     Name = "ec2-created-by-tf"
