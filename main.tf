b960fdbcaaec69b0449b6fe165c0fdb51311022e
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 14:27:37 -0300

    optimizing scripts

--- main.tf
+++ main.tf
@@ -1,37 +1,12 @@
-
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "terraform-pipeline-itau"
-
-  logging {
-        target_bucket = "target-bucket"
-  }
-  versioning {
-    enabled = true
-  }
+provider "aws" {
+  region = "us-east-2"                         
 }
 
-resource "aws_s3_bucket_public_access_block" "tf_state_public_access" {
-  bucket = aws_s3_bucket.tf_state.id
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"       
+  instance_type = "t3.micro"
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_kms_key" "tf_state_key" {
-  description             = "KMS key for tf_state bucket encryption"
-  enable_key_rotation     = true
-  deletion_window_in_days = 7
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
-  bucket = aws_s3_bucket.tf_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.tf_state_key.arn
-      sse_algorithm     = "aws:kms"
-    }
+  tags = {
+    Name = "git-pipeline-tf-vm"
   }
 }
