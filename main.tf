c2544ec50366c05c728a9a9a83fb0d9a087d90c8
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 13:33:07 -0300

    v4

--- main.tf
+++ main.tf
@@ -1,44 +1,72 @@
+resource "aws_s3_bucket" "log_storage" {
+  bucket = "terraform-pipeline-itau-access-logs"
 
-provider "aws" {
-  region = "us-east-2"
+  versioning {
+    enabled = true
+  }
 }
-resource "aws_kms_key" "tf_state" {
-  description             = "KMS key for Terraform state bucket"
+
+resource "aws_s3_bucket_public_access_block" "log_storage_public_access" {
+  bucket = aws_s3_bucket.log_storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "log_storage_key" {
+  description             = "KMS key for log_storage bucket encryption"
   enable_key_rotation     = true
   deletion_window_in_days = 7
 }
 
-resource "aws_s3_bucket" "log_storage" {
-  bucket = "terraform-pipeline-itau-access-logs"
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_storage_encryption" {
+  bucket = aws_s3_bucket.log_storage.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.log_storage_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
+
 resource "aws_s3_bucket" "tf_state" {
   bucket = "terraform-pipeline-itau"
+
   logging {
     target_bucket = aws_s3_bucket.log_storage.bucket
     target_prefix = "access-logs/"
   }
+
   versioning {
     enabled = true
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
-resource "aws_s3_bucket_public_access_block" "block" {
+resource "aws_s3_bucket_public_access_block" "tf_state_public_access" {
   bucket = aws_s3_bucket.tf_state.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "tf_state_key" {
+  description             = "KMS key for tf_state bucket encryption"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.tf_state_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
