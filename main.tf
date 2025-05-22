61136de1c0a645e0120d07d746abc025d4a2997b
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 12:54:27 -0300

    scripts updated

--- main.tf
+++ main.tf
@@ -1,42 +1,24 @@
-resource "aws_s3_bucket" "caique-s3" {
-  bucket = "pipeline-tf-githubactions"
-
-  logging {
-    target_bucket = aws_s3_bucket.log_bucket.id
-    target_prefix = "log/"
-  }
-}
-
 resource "aws_s3_bucket" "log_bucket" {
-  bucket = "pipeline-tf-githubactions-logs"
-  
-  acl = "log-delivery-write"
-
-  # Optional: to block public access on log bucket as well
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
+  bucket = "terraform-pipeline-itau"
 }
 
-resource "aws_s3_bucket_ownership_controls" "caique-ownership-controls" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.log_bucket.id
+
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "caique-acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership]
 
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.log_bucket.id
   acl    = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "caique-public-access-block" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.log_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -44,8 +26,9 @@ resource "aws_s3_bucket_public_access_block" "caique-public-access-block" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.log_bucket.id
+
   versioning_configuration {
     status = "Enabled"
   }
@@ -57,43 +40,13 @@ resource "aws_kms_key" "mykey" {
   deletion_window_in_days = 7
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "caique-encryption" {
-  bucket = aws_s3_bucket.example.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.log_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.mykey.arn
       sse_algorithm     = "aws:kms"
     }
-  }
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-
-  root_block_device {
-    encrypted   = true
-    volume_size = 8      # Optional: adjust as needed
-    volume_type = "gp3"  # Optional: adjust as needed
-  }
-
-  metadata_options {
-    http_tokens   = "required"
-    http_endpoint = "enabled"
-  }
-
-  tags = {
-    Name = "ec2-created-by-tf"
   }
 }
