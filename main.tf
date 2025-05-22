cf9fc949c7eff4bbfab14c5ba94712937dda92be
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 14:29:27 -0300

    adding ec2

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-0c55b159cbfafe1f0"       
   instance_type = "t3.micro"
 
+  root_block_device {
+    volume_size = 8             
+    volume_type = "gp3"    
+    encrypted   = true             
+  }
+
   tags = {
     Name = "git-pipeline-tf-vm"
   }
