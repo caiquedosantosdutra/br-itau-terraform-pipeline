888cca5e7a6fccf810e5bf1afb2dc8b0fb633026
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 14:19:53 -0300

    v5

--- main.tf
+++ main.tf
@@ -1,45 +1,10 @@
-resource "aws_s3_bucket" "log_storage" {
-  bucket = "terraform-pipeline-itau-access-logs"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "log_storage_public_access" {
-  bucket = aws_s3_bucket.log_storage.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_kms_key" "log_storage_key" {
-  description             = "KMS key for log_storage bucket encryption"
-  enable_key_rotation     = true
-  deletion_window_in_days = 7
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "log_storage_encryption" {
-  bucket = aws_s3_bucket.log_storage.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.log_storage_key.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
 
 resource "aws_s3_bucket" "tf_state" {
   bucket = "terraform-pipeline-itau"
 
   logging {
-    target_bucket = aws_s3_bucket.log_storage.bucket
-    target_prefix = "access-logs/"
+        target_bucket = "target-bucket"
   }
-
   versioning {
     enabled = true
   }
