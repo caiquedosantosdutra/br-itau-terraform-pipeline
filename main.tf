736b85b6c28c78ad34b0109c75d044371ce44934
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 14:35:59 -0300

    adding subnet

--- main.tf
+++ main.tf
@@ -2,25 +2,90 @@ provider "aws" {
   region = "us-east-2"                         
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"       
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.sg.id]
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main"
+  }
+}
 
-  metadata_options {
-    http_endpoint               = "enabled"       
-    http_tokens                 = "required"      
-    http_put_response_hop_limit = 1               
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_route_table" "rtable" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "main-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rtable.id
+}
+
+resource "aws_security_group" "sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  root_block_device {
-    volume_size = 8             
-    volume_type = "gp3"    
-    encrypted   = true             
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "git-pipeline-tf-vm"
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                         = "ami-0c55b159cbfafe1f0"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  associate_public_ip_address = true
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+    encrypted   = true
+  }
+
+  tags = {
+    Name = "ec2-instance"
   }
 }
