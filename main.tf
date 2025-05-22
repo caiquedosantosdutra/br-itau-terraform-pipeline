2ab6756dff1eb832ea63e57c142391ec487afe94
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 14:56:08 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "ec2_instance" {
-  ami                         = "ami-06aa3f7caf3a30282"
+  ami                         = "ami-06c8f2ec674c67112"
   instance_type               = "t2.micro"
 
 
