410f757418f1c8d272ed543ee2b46f48a404c3ed
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-22 13:23:14 -0300

    storing tf state on bucket

--- main.tf
+++ main.tf
@@ -1,105 +1,32 @@
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "terraform-pipeline-itau"
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket_ownership_controls" "ownership" {
-  bucket = aws_s3_bucket.log_bucket.id
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-pipeline-itau"
 
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
-resource "aws_s3_bucket_acl" "acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.ownership]
-
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.log_bucket.id
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.tf_state.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_kms_key" "mykey" {
-  description             = "KMS key for S3 bucket encryption"
-  enable_key_rotation     = true
-  deletion_window_in_days = 7
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
-resource "aws_cloudtrail" "s3_trail" {
-  name                          = "s3-access-logs"
-  s3_bucket_name                = aws_s3_bucket.log_bucket.id
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_logging                = true
-
-  event_selector {
-    read_write_type           = "All"
-    include_management_events = true
-
-    data_resource {
-      type   = "AWS::S3::Object"
-      values = ["arn:aws:s3:::${aws_s3_bucket.log_bucket.bucket}/"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "log_bucket_policy" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid = "AWSCloudTrailAclCheck"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        }
-        Action = "s3:GetBucketAcl"
-        Resource = "arn:aws:s3:::${aws_s3_bucket.log_bucket.bucket}"
-      },
-      {
-        Sid = "AWSCloudTrailWrite"
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudtrail.amazonaws.com"
-        }
-        Action = "s3:PutObject"
-        Resource = "arn:aws:s3:::${aws_s3_bucket.log_bucket.bucket}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
-      }
-    ]
-  })
-}
-
-data "aws_caller_identity" "current" {}
