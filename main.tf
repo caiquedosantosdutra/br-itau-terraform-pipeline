50a03d5ed9ba23b6c30d515194983ef1dc782596
Caique22 <56844645+Caique22@users.noreply.github.com>
2025-05-23 10:07:50 -0300

    fixing ec2 issues

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "ec2_instance" {
+resource "aws_instance" "ec2_instance_itau" {
   ami                         = "ami-06c8f2ec674c67112"
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-09f35be1a6f0f024b"
