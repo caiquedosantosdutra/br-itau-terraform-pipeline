5bc996392725b50925fbae91461e56300051e2d8
Caique Dutra Santos <caique.santos@ibm.com>
2025-06-04 10:20:21 -0300

    adding hcp tf state file config

--- backend.tf
+++ backend.tf
@@ -1,16 +1,14 @@
 terraform {
-    backend "s3" {
-    bucket         = "terraform-pipeline-itau"
-    region         = "us-east-2"
-    key            = "terraform.tfstate"
-    encrypt = true
-  }
-
+cloud {
+    organization = "poc-itau-org"
+    workspaces {
+      name = "itau-pipeline-test-bruno"
+    }
+   }
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
       version = "~> 3.65.0"
-
+      source = "hashicorp/aws"
     }
   }
 }
