588a4ddbe181bbcdfe945dd1af3bbefe99234bf4
Caique Dutra Santos <caique.santos@ibm.com>
2025-06-04 10:15:56 -0300

    adding s3 tf state file config

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-   cloud {
-      organization = "poc-itau-org"
-      workspaces {
-        tags = ["itau-pipeline-workspace"]
-      }
-    }
+    backend "s3" {
+    bucket         = "terraform-pipeline-itau"
+    region         = "us-east-2"
+    key            = "terraform.tfstate"
+    encrypt = true
+  }
 
   required_providers {
     aws = {
