0bedd3c416004d2fae6030e352d17743325851d4
Caique Dutra Santos <caique.santos@ibm.com>
2025-06-04 14:50:20 -0300

    backend s3 v1

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-cloud {
-  organization = "poc-itau-org"
-    workspaces {
-      name = "itau-pipeline-test-bruno"
-    }
-   }
+  backend "s3" {
+    bucket         = "terraform-pipeline-itau"
+    region         = "us-east-2"
+    key            = "terraform.tfstate"
+    encrypt = true
+  }
   required_providers {
     aws = {
       version = "~> 3.65.0"
